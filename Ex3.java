f05a0922190e14ea5f29cd696e70d47082816102
subseven <daniel@subseven.io>
2019-03-16 16:19:41 -0500

    Finished exercise 3

---
+++ Ex3.java
@@ -0,0 +1,11 @@
+public class Ex3 {
+public static void main(String[] args) {
+  float numberOne = 50;
+  float numerTwo = 3;
+
+  System.out.print(numberOne / numerTwo + " Or ");
+  System.out.print(50 / 3 + "\n");
+
+}
+
+}   //End of class
