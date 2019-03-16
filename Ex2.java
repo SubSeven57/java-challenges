f5a476cd900ac414fdd44f28ae768a85ad90e8db
subseven <daniel@subseven.io>
2019-03-16 16:12:51 -0500

    Finished exercise 2

---
+++ Ex2.java
@@ -0,0 +1,11 @@
+public class Ex2 {
+public static void main(String[] args) {
+  int numberOne = 74;
+  int numerTwo = 36;
+
+  System.out.print(numberOne + numerTwo + " Or ");
+  System.out.print(74 + 36 + "\n");
+
+}
+
+}   //End of class
