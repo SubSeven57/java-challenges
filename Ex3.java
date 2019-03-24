2874e4797e9d713296d8a650d3575064b31dbc2a
subseven <daniel@subseven.io>
2019-03-23 20:37:59 -0500

    Organization

--- Ex3.java
+++
@@ -1,7 +0,0 @@
-// 3. Write a Java program to divide two numbers and print on the screen.
-
-public class Ex3 {
-public static void main(String[] args) {
-  System.out.print(50 / 3 + "\n");
-  }
-}   //End of class
