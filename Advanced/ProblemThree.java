48f37ab5418d6394487c39f1b1ce1582097f058d
subseven <daniel@subseven.io>
2019-03-23 20:37:05 -0500

    Added new folder for advanced java problems and commiting problem 3

---
+++ Advanced/ProblemThree.java
@@ -0,0 +1,44 @@
+/*
+Write a Java program that prompts the user to enter integers from the keyboard one
+at a time. The program stops reading integers once the user enters the same value
+three times consecutively (meaning three times, one after the other). Once input
+is completed the program is to display the message "Same entered 3 in a row"
+*/
+
+import java.util.Scanner;
+
+public class ProblemThree {
+public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        int input;
+        int temp;
+        int counter = 1;
+
+        boolean run = true;
+
+        System.out.print("Enter an interger: ");
+        input = keyboard.nextInt();
+        temp = input;
+        do {
+                System.out.print("Enter an interger: ");
+                input = keyboard.nextInt();
+
+                if (input == temp) {
+                        counter++;
+                }
+
+                else {
+                        counter = 1;
+                }
+
+                temp = input;
+
+                if (counter == 3) {
+                        System.out.println("Same interger 3 times in a row");
+                        run = false;
+                }
+
+        } while(run == true);
+}
+}
